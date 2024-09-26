0d1aa99480392fc3b820575f1a3d5571f9e07403
Divindivine <divinparakalayil@gmail.com>
2024-09-26 08:39:43 +0530

    25/09/2024

---
+++ 100-questions/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Questions from "./components/Questions";
+
+function App() {
+  return (
+    <div className="main">
+      <div className="mainInner">
+        < Questions />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 100-questions/src/components/Questions.tsx
@@ -0,0 +1,29 @@
+import "./Questions.css";
+import classObj from "../Data";
+import { useState } from "react";
+
+function Questions() {
+  let value;
+
+  return (<>
+    <div className="eachQuestion">
+      <span className="question">
+        hai hai hiah hia hiahihai ihahia ihah iahahi
+      </span>
+    </div>
+    <div className="eachQuestion">
+    <span className="question">
+      hai hai hiah hia hiahihai ihahia ihah iahahi
+    </span>
+  </div>
+
+<div className="eachQuestion">
+      <span className="question">
+        hai hai hiah hia hiahihai ihahia ihah iahahi
+      </span>
+    </div>
+    </>
+  );
+}
+
+export default Questions;
